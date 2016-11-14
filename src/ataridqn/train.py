ee0a8e8d6456544dcceeef945fc57f4510b5cfa7
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-11-14 14:32:08 +0100

    resolution should not be 0

--- src/ataridqn/train.py
+++ src/ataridqn/train.py
@@ -1,12 +1,11 @@
 import gym
-
-# init environment
 from src.ataridqn.agent import Agent
 
+# init environment
 env = gym.make('Pong-v0')
 
 # init agent
-agent = Agent(env, colors=False, scale=0, cropping=(0, 0, 0, 0))
+agent = Agent(env, colors=False, scale=1, cropping=(0, 0, 0, 0))
 #agent = Agent(env, colors=False, scale=.5, cropping=(30, 10, 6, 6))
 # train agent on the environment
 agent.learn(render_training=False, render_test=False)
