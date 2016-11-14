2cba41ce715af456aa57048ead274d911f740514
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-11-14 15:01:57 +0100

    cropping again

--- src/ataridqn/train.py
+++ src/ataridqn/train.py
@@ -5,7 +5,7 @@ from agent import Agent
 env = gym.make('Pong-v0')
 
 # init agent
-agent = Agent(env, colors=False, scale=1, cropping=(0, 0, 0, 0))
+agent = Agent(env, colors=False, scale=1, cropping=(30, 10, 6, 6))
 #agent = Agent(env, colors=False, scale=.5, cropping=(30, 10, 6, 6))
 # train agent on the environment
 agent.learn(render_training=False, render_test=False)
