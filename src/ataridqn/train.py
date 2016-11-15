f079ae582443b0d9b5d56b75ea0baefcd28f3199
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-11-15 09:51:24 +0100

    FINT!

--- src/ataridqn/train.py
+++ src/ataridqn/train.py
@@ -5,7 +5,7 @@ from agent import Agent
 env = gym.make('Pong-v0')
 
 # init agent
-agent = Agent(env, batch_size=128, replay_memory_size=312500, colors=False, scale=1, cropping=(30, 10, 6, 6))
+agent = Agent(env, batch_size=64, replay_memory_size=312500, colors=False, scale=1, cropping=(30, 10, 6, 6))
 # agent = Agent(env, colors=False, scale=.5, cropping=(30, 10, 6, 6))
 # train agent on the environment
 agent.learn(epochs=500, render_training=False, render_test=False)
