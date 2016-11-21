Fixed everything now, SQren Softmax

src/ataridqn/agent.py
@@ -178,10 +178,10 @@ class Agent(object):
             # plt.imshow(img)
             # img = skimage.color.rgb2gray(img)
             # plt.imshow(img, cmap=plt.cm.gray)
-            # img = img[np.newaxis, ...]
+
         # else:
         #     img = img.reshape(self.channels, self.resolution[0], self.resolution[1])
-
+        img = img[np.newaxis, ...]
         img = img.astype(np.float32)
 
         return img